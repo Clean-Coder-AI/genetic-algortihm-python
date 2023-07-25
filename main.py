c48a0a16bff433f3dc32d82b14a61f638d4e9ef9
Swopnil Sapkota <59927837+Swopnil-Sapkota@users.noreply.github.com>
2023-07-25 17:52:34 +0545

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from GeneticAlgorithm import SimpleGeneticAlgorithm
+from genetic_algorithm import SimpleGeneticAlgorithm
 
-if __name__=="__main__":
-    population_size=50
-    solution="1011000100000100010000100000100111001000000100000100000000001111"
-    ga=SimpleGeneticAlgorithm(solution, population_size)
+if __name__ == "__main__":
+    population_size = 50
+    solution = "1011000100000100010000100000100111001000000100000100000000001111"
+    ga = SimpleGeneticAlgorithm(solution, population_size)
     ga.run_algorithm()
+
+    
