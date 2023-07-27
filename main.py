a65ecf5a60bb7acd33661fa760269f532352f9ef
Sarwesh13 <87237949+Sarwesh13@users.noreply.github.com>
2023-07-27 12:59:05 +0545

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from genetic_algorithm import SimpleGeneticAlgorithm
-
-if __name__ == "__main__":
-    population_size = 50
-    solution = "1011000100000100010000100000100111001000000100000100000000001111"
-    ga = SimpleGeneticAlgorithm(solution, population_size)
-    ga.run_algorithm()
-
-    
