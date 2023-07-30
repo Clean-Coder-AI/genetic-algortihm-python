e71e63b8b56934a395a29271481bceecad4f0381
Sarwesh Kattel <075bct013.sarwesh@sagarmatha.edu.np>
2023-07-30 14:54:15 +0545

    Proper unittest by mocking

--- test/test_GeneticAlgorithm.py
+++ test/test_GeneticAlgorithm.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import unittest
+import random
+
+from itertools import cycle
+
 from unittest.mock import patch
 
-sys.path.append(r'C:\Users\user\Desktop\Work')
-
-os.chdir(r'C:\Users\user\Desktop\Work\GeneticAlgorithm')
+sys.path.append('C:/Users/Sarwe/OneDrive/Desktop/Non-Spaghetti')
 
 from GeneticAlgorithm.GeneticAlgorithm import SimpleGeneticAlgorithm
 from GeneticAlgorithm.Population import Population
@@ -18,112 +20,85 @@ class TestSimpleGeneticAlgorithm(unittest.TestCase):
         self.ga = SimpleGeneticAlgorithm(self.target_solution, self.population_size)
 
     def test_crossover(self):
-        # Test case 1: Crossover between parent1 with all 1s and parent2 with all 0s
-        parent1 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        parent1.set_single_gene(0, 1)
-        parent1.set_single_gene(1, 1)
-        parent1.set_single_gene(2, 1)
-        parent1.set_single_gene(3, 1)
-        parent1.set_single_gene(4, 1)
-
-        parent2 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        parent2.set_single_gene(0, 0)
-        parent2.set_single_gene(1, 0)
-        parent2.set_single_gene(2, 0)
-        parent2.set_single_gene(3, 0)
-        parent2.set_single_gene(4, 0)
-
-        expected_child_1 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        expected_child_1.set_single_gene(0, 1)
-        expected_child_1.set_single_gene(1, 1)
-        expected_child_1.set_single_gene(2, 1)
-        expected_child_1.set_single_gene(3, 1)
-        expected_child_1.set_single_gene(4, 1)
-
-        with patch.object(self.ga, 'crossover', return_value=expected_child_1) as mock_crossover:
-            child_1 = self.ga.crossover(parent1, parent2)
-            self.assertEqual(child_1, expected_child_1)
-            mock_crossover.assert_called_once_with(parent1, parent2)
-
-        # Test case 2: Crossover between parent1 with all 0s and parent2 with all 1s
+         # Create two parent individuals for crossover
+        parent1 = Individual(self.target_solution)  
         parent1.set_single_gene(0, 0)
-        parent1.set_single_gene(1, 0)
+        parent1.set_single_gene(1, 1)
         parent1.set_single_gene(2, 0)
-        parent1.set_single_gene(3, 0)
-        parent1.set_single_gene(4, 0)
+        parent1.set_single_gene(3, 1)
+        parent1.set_single_gene(4, 0)  #[0, 1, 0, 1, 0]
 
+        parent2 = Individual(self.target_solution)  
         parent2.set_single_gene(0, 1)
         parent2.set_single_gene(1, 1)
-        parent2.set_single_gene(2, 1)
+        parent2.set_single_gene(2, 0)
         parent2.set_single_gene(3, 1)
-        parent2.set_single_gene(4, 1)
+        parent2.set_single_gene(4, 0) #[1, 1, 0, 1, 0]
 
-        expected_child_2 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        expected_child_2.set_single_gene(0, 0)
-        expected_child_2.set_single_gene(1, 0)
-        expected_child_2.set_single_gene(2, 0)
-        expected_child_2.set_single_gene(3, 0)
-        expected_child_2.set_single_gene(4, 0)
+        # Set the value to be returned by the mock random_gen.random() method
+        # if random_gen.random()<SimpleGeneticAlgorithm.uniform_rate: this condition is set false
+        mock_random_value = SimpleGeneticAlgorithm.uniform_rate + 0.1
 
-        with patch.object(self.ga, 'crossover', return_value=expected_child_2) as mock_crossover:
-            child_2 = self.ga.crossover(parent1, parent2)
-            self.assertEqual(child_2, expected_child_2)
-            mock_crossover.assert_called_once_with(parent1, parent2)
+        # Use the patch decorator to mock the random_gen.random() method
+        with patch.object(random, 'Random') as mock_random_gen:
+            random_gen_instance = mock_random_gen.return_value
+            random_gen_instance.random.return_value = mock_random_value
+
+            # Perform crossover with the mocked random_gen.random()
+            child = self.ga.crossover(parent1, parent2)
+
+        # The child should inherit the genes from parent2 since the condition is always false
+        self.assertEqual(child.genes, parent2.genes)
 
     def test_mutate(self):
-        # Test case 1: Mutate an individual with all 1s
-        individual_1 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        individual_1.set_single_gene(0, 1)
-        individual_1.set_single_gene(1, 1)
-        individual_1.set_single_gene(2, 1)
-        individual_1.set_single_gene(3, 1)
-        individual_1.set_single_gene(4, 1)
-        with patch.object(self.ga, 'mutate', return_value=individual_1) as mock_mutate:
-            mutated_individual_1 = self.ga.mutate(individual_1)
-            self.assertEqual(mutated_individual_1, individual_1)
-            mock_mutate.assert_called_once_with(individual_1)
+        # Create an individual for mutation
+        individual = Individual(self.target_solution)  
+        individual.set_single_gene(0, 0)
+        individual.set_single_gene(1, 1)
+        individual.set_single_gene(2, 1)
+        individual.set_single_gene(3, 1)
+        individual.set_single_gene(4, 0)  #[0, 1, 1, 1, 0]
 
-        # Test case 2: Mutate an individual with all 0s
-        individual_2 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        individual_2.set_single_gene(0, 0)
-        individual_2.set_single_gene(1, 0)
-        individual_2.set_single_gene(2, 0)
-        individual_2.set_single_gene(3, 0)
-        individual_2.set_single_gene(4, 0)
-        with patch.object(self.ga, 'mutate', return_value=individual_2) as mock_mutate:
-            mutated_individual_2 = self.ga.mutate(individual_2)
-            self.assertEqual(mutated_individual_2, individual_2)
-            mock_mutate.assert_called_once_with(individual_2)
+        # Set the value to be returned by the mock random_gen.random() method
+        mock_random_value = SimpleGeneticAlgorithm.mutation_rate - 0.1
 
-        # Test case 3: Mutate an individual with mixed values
-        individual_3 = Individual(self.target_solution)  # [1, 0, 1, 0, 1]
-        individual_3.set_single_gene(0, 0)
-        individual_3.set_single_gene(1, 1)
-        individual_3.set_single_gene(2, 0)
-        individual_3.set_single_gene(3, 1)
-        individual_3.set_single_gene(4, 0)
-        with patch.object(self.ga, 'mutate', return_value=individual_3) as mock_mutate:
-            mutated_individual_3 = self.ga.mutate(individual_3)
-            self.assertEqual(mutated_individual_3, individual_3)
-            mock_mutate.assert_called_once_with(individual_3)
+        # Use the patch decorator to mock the random_gen.random() method
+        with patch.object(random, 'Random') as mock_random_gen:
+            random_gen_instance = mock_random_gen.return_value
+            random_gen_instance.random.return_value = mock_random_value
+
+            # Perform mutation with the mocked random_gen.random()
+            self.ga.mutate(individual)
+
+        # All genes should be flipped since the condition is always true
+        expected_genes = [1, 0, 0, 0, 1]
+        self.assertEqual(individual.genes, expected_genes)
 
     def test_tournament_selection(self):
         # Create a population with specific individuals and their fitness values
-        population = Population(5, self.target_solution, initialize=True)
-        population.get_individual(0).set_single_gene(0, 0)  # Fitness: 0
-        population.get_individual(1).set_single_gene(0, 0)  # Fitness: 0
-        population.get_individual(2).set_single_gene(0, 1)  # Fitness: 1
-        population.get_individual(3).set_single_gene(0, 0)  # Fitness: 0
-        population.get_individual(4).set_single_gene(0, 0)  # Fitness: 0
+        population = Population(self.population_size, self.target_solution, initialize=True)
+        population.get_individual(0).set_single_gene(0, 0)
+        population.get_individual(1).set_single_gene(0, 0)
+        population.get_individual(2).set_single_gene(0, 1)
+        population.get_individual(3).set_single_gene(0, 0)
+        population.get_individual(4).set_single_gene(0, 0)
 
-        # We expect the tournament winner to be the individual with the highest fitness (gene value = 1)
-        expected_winner = population.get_individual(2)
+        # Set the indices to be returned by the mock random_gen.randint() method
+        mock_random_values = cycle([0, 2, 4])
 
-        # Replace the `tournament_selection` method with a mock that returns the expected winner
-        with patch.object(self.ga, 'tournament_selection', return_value=expected_winner) as mock_tournament_selection:
+        # Use the patch decorator to mock the random_gen.randint() method
+        with patch.object(random, 'Random') as mock_random_gen:
+            random_gen_instance = mock_random_gen.return_value
+            random_gen_instance.randint.side_effect = lambda a, b: next(mock_random_values)
+
+            # Perform tournament selection with the mocked random_gen.randint()
             tournament_winner = self.ga.tournament_selection(population)
-            self.assertEqual(tournament_winner, expected_winner)
-            mock_tournament_selection.assert_called_once_with(population)
+
+        # The tournament winner should be the individual with the highest fitness (gene value = 1),
+        # which is the third individual in this case
+        third_individual_genes = population.get_individual(2).genes
+        tournament_winner_genes = tournament_winner.genes
+        self.assertTrue(tournament_winner_genes == third_individual_genes)
 
     def test_run_algorithm(self):
         pass
